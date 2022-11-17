951618a738ea8da7290f2677b95ba49bcea719ca
Mateen Faieq <faieqma@dukes.jmu.edu>
2022-11-16 20:11:25 -0500

    Add SAM template

--- back-end/lambda/mtb-weather-app/hello_world/app.py
+++ back-end/lambda/mtb-weather-app/hello_world/app.py
@@ -1,16 +1,55 @@
-
-
-from decimal import Decimal
-from flask import Flask, jsonify, request
-from flask import render_template
+import json
 from bs4 import BeautifulSoup
 from enum import Enum
-from flask_cors import CORS, cross_origin
 import requests
+from decimal import Decimal
+
+# import requests
 
 
-app = Flask(__name__)
-CORS(app, support_credentials=True)
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+    try:
+        print("This is a test")
+        data = get_park_trail_condition_and_forecast_details()
+        print(json.dumps(data))
+        print(data)
+    except Exception as e:
+        print("Another test")
+        print(e)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(get_park_trail_condition_and_forecast_details())
+    }
+
 
 location_name_to_latlong_map = {
     'massanutten' : 'LWX/40,42',
@@ -21,21 +60,23 @@ def get_forecast_descriptions(location_name):
     grid_value = location_name_to_latlong_map[location_name]
 
     url = "https://api.weather.gov/gridpoints/" + grid_value + "/forecast"
+    print("Last test")
     response_json = requests.get(url).json()
     print(response_json)
     daily_forecast_list = response_json
     return daily_forecast_list["properties"]["periods"][0:4]
     
 
-@app.route("/")
-@cross_origin(supports_credentials=True)
 def get_park_trail_condition_and_forecast_details():
+    print("Test before first request")
     r = requests.get("https://www.localconditions.com/weather-massanutten-virginia/va281/past.php")
     soup = BeautifulSoup(r.text, 'html.parser')
     x = soup.find("div", {"id": "accordion-paneled"})
     children = x.findChildren("div", recursive=False)
     rain_list = list()
     listSize = 0
+
+    print("Test 2")
 
     for child in children:
         rainfall_amount = child.find_all("span")[-1].next_sibling
@@ -54,6 +95,7 @@ def get_park_trail_condition_and_forecast_details():
 
     trail_conditions_enum = determine_trail_condition(rain_list)
 
+    print("Test 3")
     forecast_descriptions_list = get_forecast_descriptions("massanutten")
     print(forecast_descriptions_list)
     data = { 
@@ -61,7 +103,7 @@ def get_park_trail_condition_and_forecast_details():
         'trail_condition_description' : trail_conditions_enum.value,
         'forecast_descriptions' : forecast_descriptions_list
     }
-    return jsonify(data)
+    return data
 
 def determine_trail_condition(recent_precipitation_list):
     sumVal = sum(recent_precipitation_list)
